01b2930fc3337b1c65ddc322234e7fa295ab585a
x13319 <1191838499@qq.com>
2019-12-12 15:04:12 +0800

    master

--- src/main/java/com/cn/xu/controller/Master.java
+++ src/main/java/com/cn/xu/controller/Master.java
@@ -1,5 +1,15 @@
 package com.cn.xu.controller;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
 public class Master {
+	
+	
+	public void insert(){
+		
+	}
 
 }
